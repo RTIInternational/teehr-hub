5c56133f60ebbe914331ba1934d34529fc2d57e4
samlamont <sam.lamont@gmail.com>
2025-10-10 16:40:06 -0400

    add poetry env for teehr and migrations template

--- warehouse_management/apply_migrations.py
+++ warehouse_management/apply_migrations.py
@@ -1,8 +1,16 @@
 # Example of how to apply migrations in TEEHR
 import teehr
 
-ev = teehr.Evaluation(dir_path="evaluation")
+ev = teehr.Evaluation(
+    local_warehouse_dir="warehouse_template",
+    create_local_dir=False,
+    check_evaluation_version=False
+)
 
-ev.set_active_catalog("remote")   
+# Switch to the remote catalog.
+ev.set_active_catalog("remote")
 
+# Apply any pending migrations. This will check to see if a "migrations"
+# directory exists in the warehouse dir and apply any new migration
+# scripts found there.
 ev.apply_migrations()
