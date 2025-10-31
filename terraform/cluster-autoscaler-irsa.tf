ffce18f9e8d1b7361135d5e9932f4e7c845fe58b
Matt Denno <mgdenno@gmail.com>
2025-10-31 15:54:38 -0400

    fix autoscaler irsa

--- terraform/cluster-autoscaler-irsa.tf
+++ terraform/cluster-autoscaler-irsa.tf
@@ -1,7 +1,6 @@
 # IRSA for Cluster Autoscaler
 
 # 1. Trust policy for the Cluster Autoscaler service account
-
 data "aws_iam_policy_document" "cluster_autoscaler_trust_policy" {
   statement {
     effect  = "Allow"
@@ -20,6 +19,35 @@ data "aws_iam_policy_document" "cluster_autoscaler_trust_policy" {
   }
 }
 
+
+# Create the IAM policy document for Cluster Autoscaler
+data "aws_iam_policy_document" "cluster_autoscaler" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeInstanceTypes",
+      "eks:DescribeNodegroup",
+      "eks:ListNodegroups",
+      "eks:ListClusters"
+    ]
+    resources = ["*"]
+  }
+}
+
+# Create the IAM policy for Cluster Autoscaler
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name_prefix = "ClusterAutoscalerPolicy"
+  description = "EKS cluster-autoscaler policy for cluster ${local.cluster_name}"
+  policy      = data.aws_iam_policy_document.cluster_autoscaler.json
+}
+
 # 2. IAM role for Cluster Autoscaler
 resource "aws_iam_role" "cluster_autoscaler_irsa" {
   name               = "ClusterAutoscalerIRSA"
@@ -30,8 +58,7 @@ resource "aws_iam_role" "cluster_autoscaler_irsa" {
 # 3. Attach the Cluster Autoscaler policy
 resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
   role       = aws_iam_role.cluster_autoscaler_irsa.name
-#   policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
-  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+  policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
 }
 
 # 4. Helm chart config:
