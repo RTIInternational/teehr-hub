0fe4d3f26b143635cfb06416a8e54dc3dcb037f9
samlamont <sam.lamont@gmail.com>
2025-11-12 08:17:04 -0500

    fetch by location using prefect submit

--- prefect-workflows/workflows/02_usgs_streamflow_obs.py
+++ prefect-workflows/workflows/02_usgs_streamflow_obs.py
@@ -28,7 +28,7 @@ logging.getLogger("teehr").setLevel(logging.INFO)
 CURRENT_DT = datetime.now()
 LOOKBACK_DAYS = 1
 DEFAULT_START_DT = CURRENT_DT - timedelta(days=1)
-CHUNK_SIZE = 100  # Number of sites to fetch at once
+# CHUNK_SIZE = 100  # Number of sites to fetch at once
 
 
 @flow(flow_run_name="ingest-usgs-streamflow-obs")
@@ -37,7 +37,7 @@ def ingest_usgs_streamflow_obs(
     end_dt: Union[str, datetime, pd.Timestamp] = CURRENT_DT,
     num_lookback_days: Union[int, None] = LOOKBACK_DAYS,
     service: USGSServiceEnum = "iv",
-    chunk_by: Union[USGSChunkByEnum, None] = None,
+    chunk_by: Union[USGSChunkByEnum, None] = "location_id",
     filter_to_hourly: bool = True,
     filter_no_data: bool = True,
     convert_to_si: bool = True,
@@ -47,7 +47,7 @@ def ingest_usgs_streamflow_obs(
 ) -> None:
     """USGS Streamflow Ingestion from NWIS.
 
-    - The flow will set the start date as end date
+    - Sets the start date as end date
       minus the number of lookback days.
     - End date defaults to current date and time.
     """
@@ -58,17 +58,18 @@ def ingest_usgs_streamflow_obs(
 
     ev = initialize_evaluation(dir_path=dir_path)
 
-    if (
-        not ev.fetch._configuration_name_exists(USGS_CONFIGURATION_NAME)
-    ):
-        ev.configurations.add(
-            Configuration(
-                name=USGS_CONFIGURATION_NAME,
-                type="primary",
-                description="USGS streamflow gauge observations"
-            )
-        )
+    # if (
+    #     not ev.fetch._configuration_name_exists(USGS_CONFIGURATION_NAME)
+    # ):
+    #     ev.configurations.add(
+    #         Configuration(
+    #             name=USGS_CONFIGURATION_NAME,
+    #             type="primary",
+    #             description="USGS streamflow gauge observations"
+    #         )
+    #     )
     usgs_sites = usgs_tasks.get_usgs_location_ids(ev=ev)
+
     usgs_variable_name = USGS_VARIABLE_MAPPER[VARIABLE_NAME][service]
     output_parquet_dir = Path(
         ev.fetch.usgs_cache_dir,
@@ -78,18 +79,12 @@ def ingest_usgs_streamflow_obs(
 
     start_dt = end_dt - timedelta(days=num_lookback_days)
 
-    # Split into chunks of 100
-    usgs_site_chunks = [
-        usgs_sites[i:i + CHUNK_SIZE]
-        for i in range(0, len(usgs_sites), CHUNK_SIZE)
-    ]
-    logger.info(f"📊 Split {len(usgs_sites)} USGS sites into {len(usgs_site_chunks)} chunks")
-
     remove_dir_if_exists(ev.fetch.usgs_cache_dir)
 
-    for i, chunk in enumerate(usgs_site_chunks):
-        usgs_tasks.fetch_usgs_data_to_cache.submit(
-            usgs_sites=chunk,
+    site_futures = []
+    for site in usgs_sites:
+        future = usgs_tasks.fetch_usgs_data_to_cache.submit(
+            usgs_sites=[f"{site}"],
             output_parquet_dir=output_parquet_dir,
             start_date=start_dt,
             end_date=end_dt,
@@ -100,14 +95,19 @@ def ingest_usgs_streamflow_obs(
             convert_to_si=convert_to_si,
             overwrite_output=overwrite_output,
         )
-        logger.info(f"✅ Completed fetching USGS data to cache for chunk {i + 1}/{len(usgs_site_chunks)}")
+        site_futures.append(future)
 
-        logger.info(f"⏰ Loading USGS data chunk {i + 1} from the cache")
-        ev.load.from_cache(
-            in_path=Path(ev.fetch.usgs_cache_dir),
-            write_mode=write_mode,
-            drop_duplicates=drop_duplicates,
-            table_name="primary_timeseries",
-        )
+    wait(site_futures)
+    logger.info("✅ Completed fetching USGS data to cache")
+
+    # Todo: Coalesce cache files?
+
+    logger.info("⏰ Loading USGS data from the cache")
+    ev.load.from_cache(
+        in_path=Path(ev.fetch.usgs_cache_dir),
+        write_mode=write_mode,
+        drop_duplicates=drop_duplicates,
+        table_name="primary_timeseries",
+    )
     logger.info("✅ Completed loading USGS data into the warehouse")
     ev.spark.stop()
