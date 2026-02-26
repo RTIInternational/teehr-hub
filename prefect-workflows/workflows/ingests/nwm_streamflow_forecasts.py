588c220e824cbd3a035a0e4c52c8a8058a3e5966
samlamont <sam.lamont@gmail.com>
2026-02-26 06:26:23 -0500

    increase resources

--- prefect-workflows/workflows/ingests/nwm_streamflow_forecasts.py
+++ prefect-workflows/workflows/ingests/nwm_streamflow_forecasts.py
@@ -55,9 +55,9 @@ def ingest_nwm_streamflow_forecasts(
     ev = initialize_evaluation(
         dir_path=dir_path,
         start_spark_cluster=True,
-        executor_instances=4,
-        executor_cores=4,
-        executor_memory="32g"
+        executor_instances=8,
+        executor_cores=7,
+        executor_memory="50g"
     )
 
     # Format the NWM configuration name for TEEHR
