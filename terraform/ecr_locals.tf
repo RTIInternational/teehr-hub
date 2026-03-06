20f1567050affee14287026ac3bef15b124c8b32
Matt Denno <mgdenno@gmail.com>
2026-03-06 09:23:10 -0500

    update file formatting

--- terraform/ecr_locals.tf
+++ terraform/ecr_locals.tf
@@ -1,31 +1,31 @@
 locals {
-#   ecr_policy = [
-#     {
-#       actions = [
-#         "ecr:GetAuthorizationToken",
-#         "ecr:BatchCheckLayerAvailability",
-#         "ecr:GetDownloadUrlForLayer",
-#         "ecr:GetRepositoryPolicy",
-#         "ecr:DescribeRepositories",
-#         "ecr:ListImages",
-#         "ecr:DescribeImages",
-#         "ecr:BatchGetImage",
-#         "ecr:GetLifecyclePolicy",
-#         "ecr:GetLifecyclePolicyPreview",
-#         "ecr:ListTagsForResource",
-#         "ecr:DescribeImageScanFindings",
-#         "ecr:InitiateLayerUpload",
-#         "ecr:UploadLayerPart",
-#         "ecr:CompleteLayerUpload",
-#         "ecr:PutImage"
-#       ]
-#       resources = []
-#     },
-#     {
-#       actions   = ["ecr:GetAuthorizationToken"]
-#       resources = ["*"]
-#     }
-#   ]
+  #   ecr_policy = [
+  #     {
+  #       actions = [
+  #         "ecr:GetAuthorizationToken",
+  #         "ecr:BatchCheckLayerAvailability",
+  #         "ecr:GetDownloadUrlForLayer",
+  #         "ecr:GetRepositoryPolicy",
+  #         "ecr:DescribeRepositories",
+  #         "ecr:ListImages",
+  #         "ecr:DescribeImages",
+  #         "ecr:BatchGetImage",
+  #         "ecr:GetLifecyclePolicy",
+  #         "ecr:GetLifecyclePolicyPreview",
+  #         "ecr:ListTagsForResource",
+  #         "ecr:DescribeImageScanFindings",
+  #         "ecr:InitiateLayerUpload",
+  #         "ecr:UploadLayerPart",
+  #         "ecr:CompleteLayerUpload",
+  #         "ecr:PutImage"
+  #       ]
+  #       resources = []
+  #     },
+  #     {
+  #       actions   = ["ecr:GetAuthorizationToken"]
+  #       resources = ["*"]
+  #     }
+  #   ]
 
   ecr_repos = [
     {
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,22 +1,22 @@
 resource "aws_s3_bucket" "teehr_iceberg_warehouse" {
-	bucket        = "${var.environment}-${var.project_name}-iceberg-warehouse"
+  bucket = "${var.environment}-${var.project_name}-iceberg-warehouse"
 
-	tags = {
-		Name        = "${var.environment}-${var.project_name}-iceberg-warehouse"
-		Environment = var.environment
-		Project     = var.project_name
-	}
+  tags = {
+    Name        = "${var.environment}-${var.project_name}-iceberg-warehouse"
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "teehr_iceberg_warehouse_public_access_block" {
-    bucket = aws_s3_bucket.teehr_iceberg_warehouse.id
+  bucket = aws_s3_bucket.teehr_iceberg_warehouse.id
 
-    block_public_acls        = true
-    block_public_policy      = true
-    # block_public_policy      = false
-    ignore_public_acls       = true
-    restrict_public_buckets  = true
-    # restrict_public_buckets  = false
+  block_public_acls   = true
+  block_public_policy = true
+  # block_public_policy      = false
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  # restrict_public_buckets  = false
 }
 
 # resource "aws_s3_bucket_policy" "public_read" {
