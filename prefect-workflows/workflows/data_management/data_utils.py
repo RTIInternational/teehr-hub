57b8b69e7fa336a6860db5645c49253408b30cad
samlamont <sam.lamont@gmail.com>
2026-05-14 16:41:48 -0400

    start executors for remote

--- prefect-workflows/workflows/data_management/data_utils.py
+++ prefect-workflows/workflows/data_management/data_utils.py
@@ -2,9 +2,18 @@ from workflows.utils.common_utils import table_exists
 from prefect.cache_policies import NO_CACHE
 from prefect import task, get_run_logger
 
+from pyspark.sql import DataFrame as SparkDataFrame
+
+import teehr
+
 
 @task(cache_policy=NO_CACHE)
-def write_to_warehouse(ev, sdf, table_name, write_mode=None):
+def write_to_warehouse(
+    ev: teehr.Evaluation,
+    sdf: SparkDataFrame,
+    table_name: str,
+    write_mode: str = None
+) -> None:
     """
     Helper function to write a Spark DataFrame to the warehouse.
     """
