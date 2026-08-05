cd3707220061360f9a87213fc7c3fe2a85c8265c
Brandon Kolze <jkolze@rti.org>
2026-08-05 16:18:30 -0400

    feat(ui): add TanStack Query to frontend
    Plan to migrate all server state handling from react-context to TanStack
    Query.
    
    chore: add tanstack query package
    
    feat: wrap app in QueryClientProvider
    
    chore: migrate root index to .tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './index.css';
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
