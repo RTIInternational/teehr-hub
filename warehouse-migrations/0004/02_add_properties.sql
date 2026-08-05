8fa8cf9225b2a73c4202e995cef95fc0f3731678
samland1116 <samland1116@gmail.com>
2026-08-05 17:10:23 -0500

    reorganize teehr-hub and teehr-cloud-core

---
+++ warehouse-migrations/0004/02_add_properties.sql
@@ -0,0 +1,11 @@
+ALTER TABLE locations ADD COLUMNS (
+    properties MAP<STRING, STRING> comment 'Additional properties for the location as key-value pairs'
+);
+
+ALTER TABLE location_crosswalks ADD COLUMNS (
+    properties MAP<STRING, STRING> comment 'Additional properties for the location crosswalk mapping as key-value pairs'
+);
+
+ALTER TABLE location_attributes ADD COLUMNS (
+    properties MAP<STRING, STRING> comment 'Additional properties for the location attribute record as key-value pairs'
+);
---
+++ warehouse-migrations/0007/update_primary_variable_name.sql
@@ -0,0 +1,3 @@
+UPDATE primary_timeseries
+SET variable_name = 'rainfall_hourly_mean'
+WHERE variable_name = 'rainfall_hourly_rate'
