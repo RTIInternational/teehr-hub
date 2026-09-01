2ff55260aebf50764bb4b4cc9e0b757bf9212674
Brandon Kolze <jkolze@rti.org>
2026-09-01 11:59:46 -0400

    feat: set default TSQ staleTime to 10 minutes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom/client';
 import { AuthProvider } from '@/features/auth';
 import App from './App';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 10,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
